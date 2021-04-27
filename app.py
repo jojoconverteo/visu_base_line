7679349ab1841b8d0753a8360dd01efa859bf8bd
jojoconveteo <jonathan.ndamba@converteo.com>
2021-04-27 11:04:28 +0200

    deployement v.1.1.0

--- app.py
+++ app.py
@@ -10,10 +10,12 @@ data = st.file_uploader("Upload a Dataset", type=["csv", "txt"])
 
 if data is not None:
     df = pd.read_csv(data, sep=conf.sep)
+    float_explcativite_vente = df[conf.col_baseline_vente].sum() / df[conf.col_vente].sum() 
+    float_explcativite_vente_web = df[conf.col_baseline_vente_web].sum() / df[conf.col_vente_web].sum()
 
     if st.checkbox("Show baseline by mag"):
         code_mag = list(df[conf.code_mag].unique())
-        id_mag_selected = st.select_slider('What are your favorite colors', code_mag)
+        id_mag_selected = st.select_slider('Magasin', code_mag)
 
         if id_mag_selected is not None:
             df_compare_id_mag = df.query(f"{conf.code_mag} == {id_mag_selected}")
@@ -24,6 +26,25 @@ if data is not None:
             fig = px.line(df_compare_id_mag, x=conf.col_date, y="value", color="variable")
             st.plotly_chart(fig)
 
+    if st.checkbox("Show baseline by cluster"):
+        code_cluster = list(df[conf.col_cluster].unique())
+        id_cluster_selected = st.select_slider('Cluster', code_cluster)
+
+        if id_cluster_selected is not None:
+            df_compare_cluster = df.query(f"{conf.col_cluster} == {id_cluster_selected}")
+            df_compare_cluster = pd.melt(df_compare_cluster, id_vars= conf.col_date, value_vars=conf.name_col)
+            df_compare_cluster = df_compare_cluster.groupby(by=[conf.col_date, "variable"]).sum()
+            df_compare_cluster.reset_index(inplace=True)
+
+            fig = px.line(df_compare_cluster, x=conf.col_date, y="value", color="variable")
+            st.plotly_chart(fig)
+
+
+    if st.checkbox("Show explicativite baseline"):
+        st.header('Explicativité baseline') 
+        if float_explcativite_vente is not None and float_explcativite_vente_web is not None:
+            st.subheader(f"% baseline vente web : {float_explcativite_vente}")
+            st.subheader(f"% baseline vente : {float_explcativite_vente_web}")
 
 
 
--- config.py
+++ config.py
@@ -2,7 +2,11 @@ sep = ','
 name_col = ("NB_VENTE_BASELINE", "NB_VENTES_WEB_BASELINE", "NB_VENTE", "NB_VENTES_WEB")
 code_mag = "id_mag"
 col_date = "parsed_date"
-
+col_cluster = "KmeansPCA_Labels"
+col_vente = "NB_VENTE"
+col_baseline_vente = "NB_VENTE_BASELINE"
+col_vente_web = "NB_VENTES_WEB"
+col_baseline_vente_web = "NB_VENTES_WEB_BASELINE"
 
 
 
